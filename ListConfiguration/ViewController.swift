cfd24e07973b21ad0e6cdbb9d5e180a76dfa1eec
JunYeong-J <eeot2827@gmail.com>
2024-07-19 20:50:53 +0900

    Initial Commit

---
+++ ListConfiguration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListConfiguration
+//
+//  Created by 전준영 on 7/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
